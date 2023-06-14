23ca1944ede03b0c23fba395a2a7891695628e8f
Muntazir Jahangir <muntazirjahangir@Muntazirs-MacBook-Air.local>
2023-06-14 15:07:56 +0530

    trained_ok

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
+import os
 from ultralytics import YOLO
 
 model = YOLO('yolov8n-seg.pt') 
 
-model.train(data='config.yaml', epochs=1, imgsz=640)
+model.train(data='config.yaml', epochs=10, imgsz=640)
