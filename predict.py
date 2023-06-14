a1d3b747a068918afd5846748dae9bb86ed19bef
Muntazir Jahangir <muntazirjahangir@Muntazirs-MacBook-Air.local>
2023-06-14 12:38:18 +0530

    training

---
+++ predict.py
@@ -0,0 +1,25 @@
+from ultralytics import YOLO
+
+import cv2
+
+
+model_path = '/runs/weights/last.pt'
+
+image_path = '/Users/muntazirjahangir/Smart-Water-Quality-Monitoring-System/data/images/val/WaterAlgae_8717.png'
+
+img = cv2.imread(image_path)
+H, W, _ = img.shape
+
+model = YOLO(model_path)
+
+results = model(img)
+
+for result in results:
+    for j, mask in enumerate(result.masks.data):
+
+        mask = mask.numpy() * 255
+
+        mask = cv2.resize(mask, (W, H))
+
+        cv2.imwrite('./output.png', mask)
+
